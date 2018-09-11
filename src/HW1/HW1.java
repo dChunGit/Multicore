9dc34f6da5c94eda0ab04b8e541e868109bd26ef
dChun <dchun@utexas.edu>
2018-09-10 23:15:02 -0500

    Setup package structure for HW1

---
+++ src/HW1/HW1.java
@@ -0,0 +1,7 @@
+package HW1;
+
+public class HW1 {
+    public static void main(String[] args) {
+        PIncrement.parallelIncrement(1, 2);
+    }
+}
---
+++ src/HW1/PIncrement.java
@@ -0,0 +1,8 @@
+package HW1;
+
+public class PIncrement {
+    public static int parallelIncrement(int c, int numThreads) {
+        System.out.println("TEST");
+        return 0;
+    }
+}
