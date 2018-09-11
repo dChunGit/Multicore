f61d6b5320d99c4b521b51d4faeadec2c80c798a
dChun <dchun@utexas.edu>
2018-09-10 23:23:41 -0500

    Create all files to avoid merge conflicts

--- src/HW1/PIncrement.java
+++ src/HW1/PIncrement.java
@@ -3,6 +3,11 @@ package HW1;
 public class PIncrement {
     public static int parallelIncrement(int c, int numThreads) {
         System.out.println("TEST");
+        // create all threads
+        // for each thread
+        // requestCS
+        // read c and increment
+        // releaseCS
         return 0;
     }
 }
