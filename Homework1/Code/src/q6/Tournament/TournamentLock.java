eee80955a9ad70e93d8062c03c1c79c8ddc0273e
dChun <dchun@utexas.edu>
2018-09-14 18:23:38 -0500

    Add psuedocode on what tournament might look like

--- Homework1/Code/src/q6/Tournament/TournamentLock.java
+++ Homework1/Code/src/q6/Tournament/TournamentLock.java
@@ -1,17 +1,29 @@
 package q6.Tournament;
 
 public class TournamentLock implements Lock {
-    public TournamentLock(int numThreads){
+    private int numThreads;
+    private int numGates;
+
+    public TournamentLock(int numThreads) {
         // your implementation goes here.
+        this.numThreads = numThreads;
+        this.numGates = (int) Math.ceil(Math.log(numThreads));
+        // initalize array of locks
+
     }
 
     @Override
     public void lock(int pid) {
-
+        for(int l = 0; l < numGates; l++) {
+            //based on index of thread, find its gate and try peterson's on it
+            //mark gate as locked so unlock can backtrack
+        }
     }
 
     @Override
     public void unlock(int pid) {
-
+        for(int l = 0; l < numGates; l++) {
+            //release its gates based on tracked path
+        }
     }
 }
