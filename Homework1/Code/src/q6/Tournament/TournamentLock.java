f7ccd74e5bd37289d5e66b8d0df05abffeea3b64
dChun <dchun@utexas.edu>
2018-09-14 19:24:36 -0500

    Flesh out more psuedocode on what it could look like

--- Homework1/Code/src/q6/Tournament/TournamentLock.java
+++ Homework1/Code/src/q6/Tournament/TournamentLock.java
@@ -1,22 +1,27 @@
 package q6.Tournament;
 
 public class TournamentLock implements Lock {
-    private int numThreads;
     private int numGates;
+    private int[][] lockPath;
+    private int[] gates;
 
     public TournamentLock(int numThreads) {
         // your implementation goes here.
-        this.numThreads = numThreads;
         this.numGates = (int) Math.ceil(Math.log(numThreads));
         // initalize array of locks
-
+        lockPath = new int[numThreads][numGates];
+        gates = new int[numGates];
     }
 
     @Override
     public void lock(int pid) {
+        int currentGate = (pid + numGates - 1)/2;
         for(int l = 0; l < numGates; l++) {
             //based on index of thread, find its gate and try peterson's on it
+
             //mark gate as locked so unlock can backtrack
+
+            currentGate = (currentGate-1)/2;
         }
     }
 
