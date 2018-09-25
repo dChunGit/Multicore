2ee8215b0d691f9fa03666f1836772fffd187d57
Grace <grace.zhuang97@gmail.com>
2018-09-24 22:57:06 -0500

    Finish question 3, not heavily tested

--- Homework1/Code/src/q6/SimpleTest.java
+++ Homework1/Code/src/q6/SimpleTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertTrue;
 public class SimpleTest {
     int n = 8;
 
-	// @Test
+	@Test
 	public void TestTournament() {
         int res = q6.Tournament.PIncrement.parallelIncrement(100, n);
         assertTrue("Result is " + res + ", expected result is 1200100.", res == 1200100);
