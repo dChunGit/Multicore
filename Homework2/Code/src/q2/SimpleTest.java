67cc0b5980f5a069ec117f56046ce0fbb60cf6ff
Grace <grace.zhuang97@gmail.com>
2018-09-25 00:33:34 -0500

    Finish 2.a and 2.c, not thoroughly tested

--- Homework2/Code/src/q2/SimpleTest.java
+++ Homework2/Code/src/q2/SimpleTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 public class SimpleTest {
 
-    private static final int OPERATIONS = 120000;
+    private static final int OPERATIONS = 10;
 
     @Test
     public void testFischer() {
--- Homework2/Code/src/q2/c/PIncrement.java
+++ Homework2/Code/src/q2/c/PIncrement.java
@@ -1,9 +1,72 @@
 package q2.c;
 
-public class PIncrement {
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class PIncrement implements Runnable {
+    private static int counter = 0;
+    private static int perThread = 0;
+    private static AtomicInteger tailSlot = new AtomicInteger(0);
+    private static boolean[] Available;
+    private static int threads;
 
     public static int parallelIncrement(int c, int numThreads) {
-        return -1;
+        counter = c;
+        perThread = 10 / numThreads;
+        threads = numThreads;
+        int remainder = 10 % numThreads;
+        Thread[] threads = new Thread[numThreads];
+
+        Available = new boolean[numThreads];
+        Available[0] = true;
+        for (int i = 1; i < numThreads; i++) {
+            Available[i] = false;
+        }
+
+        for(int a = 0; a < numThreads; a++) {
+            boolean addOne = false;
+            if(remainder > 0) {
+                addOne = true;
+                remainder--;
+            }
+            Thread thread = new Thread(new PIncrement(addOne));
+            System.out.println("Thread " + a);
+            thread.start();
+            threads[a] = thread;
+        }
+
+        for(int a = 0; a < numThreads; a++) {
+            try {
+                threads[a].join();
+            } catch (Exception e) {
+                System.out.println("ERROR");
+            }
+        }
+
+        return counter;
     }
 
+
+    private int myCount;
+    private int mySlot;
+
+    private PIncrement(boolean addOne) {
+        this.myCount = perThread;
+        if(addOne) this.myCount++;
+    }
+
+    @Override
+    public void run() {
+        for(int a = 0; a < this.myCount; a++) {
+            increment(this.mySlot);
+        }
+    }
+
+    private static void increment(int mySlot) {
+        mySlot = tailSlot.getAndIncrement() % threads;
+        while (!Available[mySlot]) {}
+        counter++;
+        Available[mySlot] = false;
+        Available[(mySlot + 1)%threads] = true;
+    }
 }
